e9b526b65a0400731bc6efdc4c1fdc2e72db1c04
lis <lis@LAPTOP-RAN47HJS>
2023-09-18 16:19:17 +0200

    Initial commit

---
+++ threding/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Diagnostics;
+
+#region Definitions
+var array = new int[16];
+var r = new Random();
+var array2d = new int[4, 4];
+
+#endregion
+
+Thread.CurrentThread.Name = "ForegroundThread";
+
+RandomizeArray();
+ArrayToArray2D();
+var backgroundThread = new Thread(ArrayToArray2D);
+backgroundThread.Name = "BackgroundThread";
+backgroundThread.Start();
+
+
+void ArrayToArray2D()
+{
+    Console.WriteLine($"Поток {Thread.CurrentThread.Name} начал работу.");
+    var stopWatch = new Stopwatch();
+    stopWatch.Start();
+    Array.Sort(array);
+    for (int i = 0; i < array.Length; i++)
+    {
+        array2d[i / 4, i % 4] = array[i];
+    }
+    stopWatch.Stop();
+    Console.WriteLine($"Поток {Thread.CurrentThread.Name} закончил работу.");
+    Console.WriteLine($"StopWatch: {stopWatch.ElapsedTicks}");
+    
+}
+
+void RandomizeArray()
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = r.Next(0, 100);
+    }
+}
